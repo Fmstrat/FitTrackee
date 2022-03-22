e4581538c1a02367c02824a6e0f5a25057716bd5
Fmstrat <nospam@nowsci.com>
2022-03-22 16:29:06 -0400

    Add sql migration

---
+++ fittrackee/migrations/versions/23_cd0e6cf83207_add_ascent_record.py
@@ -0,0 +1,24 @@
+"""add ascent record
+
+Revision ID: cd0e6cf83207
+Revises: e30007d681cb
+Create Date: 2022-03-22 20:21:13.661883
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd0e6cf83207'
+down_revision = 'e30007d681cb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
